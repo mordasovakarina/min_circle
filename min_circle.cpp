d194ace87ef7cc7d42d10cf76a630737818ac566
mordasovakarina <k_mordasova_k@mail.ru>
2026-05-18 11:00:54 +0300

    Delete main

--- min_circle.cpp
+++ min_circle.cpp
@@ -158,21 +158,3 @@ Circle MinimumEnclosingCircleForSegments(const std::vector<LineSegment>& segment
     std::vector<Point2D> points = extractPointsFromSegments(segments);
     return minCircle(points);
 }
-
-
-
-int main() {
-    std::cout << "=== Минимальная окружность для отрезков ===\n\n";
-    
-    LineSegment segment1 = {{0, 0}, {4, 0}};
-    LineSegment segment2 = {{2, 0}, {2, 3}};
-    
-    std::vector<LineSegment> segments = {segment1, segment2};
-    
-    Circle result = MinimumEnclosingCircleForSegments(segments);
-    
-    std::cout << "Центр: (" << result.center.x << ", " << result.center.y << ")" << std::endl;
-    std::cout << "Радиус: " << result.radius << std::endl;
-    
-    return 0;
-}
